4a6e2ad10d61b593e463ca22c2a7b9373ad2cd53
fateme <fateme.ghorbaanizadeh@gmail.com>
2026-02-11 21:41:58 +0330

    add messagebroker class and methods

--- messagebroker.h
+++ messagebroker.h
@@ -2,12 +2,30 @@
 #define MESSAGEBROKER_H
 
 #include <QObject>
+#include <QHash>
 
-class MessageBroker
+#include "Topic.h"
+
+class MessageBroker : public QObject
 {
     Q_OBJECT
 public:
     MessageBroker();
+    explicit MessageBroker(QObject* parent = nullptr);
+
+    void add_topic(QString topic_name);
+
+    Topic search_topic(QString topic_name);
+
+    void add_message(QString topic_name);
+
+    void add_sbscriber(QString topic_name);
+
+    void add_reaction(QString topic_name, QString reaction);
+
+private:
+    QHash<QString, Topic> topics;
+
 };
 
 #endif // MESSAGEBROKER_H
