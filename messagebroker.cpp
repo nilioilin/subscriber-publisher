f40b22040bb410394b4ed5536b4b16404d37acf7
fateme <fateme.ghorbaanizadeh@gmail.com>
2026-02-12 11:31:51 +0330

    writie methods (add topic, publish message, get message, subscribe message, add reaction) in messagebroker class

--- messagebroker.cpp
+++ messagebroker.cpp
@@ -1,3 +1,84 @@
 #include "messagebroker.h"
+#include "subscribe.h"
+#include "Topic.h"
+#include <qdebug.h>
+#include <QDebug>
 
-MessageBroker::MessageBroker() {}
+MessageBroker::MessageBroker(QObject* parent)
+    : QObject(parent)
+{
+
+}
+
+
+Topic* MessageBroker::search_topic(QString topic_name)
+{
+    auto it = topics.find(topic_name);
+    if (it != topics.end())
+        return &it.value();
+    return nullptr;
+}
+
+
+void MessageBroker::add_topic(QString &topic_name)
+{
+    if(topics.contains(topic_name)){
+        return;
+    }
+    else{
+        Topic topic;
+        topic.name = topic_name;
+        topics.insert("topic_name", topic);
+    }
+}
+
+
+void MessageBroker::publish_message(QString &topic_name, QString &message)
+{
+    Topic *topic = search_topic(topic_name);
+    if(topic){
+        topic->messages.enqueue(message);
+    }
+}
+
+
+QString MessageBroker::get_message(QString &topic_name)
+{
+    Topic *topic = search_topic(topic_name);
+    if(topic){
+        topic->messages.dequeue();
+    }
+    return QString();
+}
+
+
+void MessageBroker::subscribe(QString &topic_name, Subscribe *subscriber){
+    Topic *topic = search_topic(topic_name);
+    if(topic){
+        topic->subscribers.append(subscriber);
+    }
+}
+
+
+void MessageBroker::add_reaction(QString &topic_name, QString &reaction, Subscribe *subscriber)
+{
+    Topic *topic = search_topic(topic_name);
+    if(topic){
+        topic->reaction[subscriber] = reaction;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- messagebroker.h
+++ messagebroker.h
@@ -3,28 +3,26 @@
 
 #include <QObject>
 #include <QHash>
-
 #include "Topic.h"
+
 
 class MessageBroker : public QObject
 {
     Q_OBJECT
 public:
-    MessageBroker();
-    explicit MessageBroker(QObject* parent = nullptr);
+    explicit MessageBroker(QObject *parent = nullptr);
 
-    void add_topic(QString topic_name);
+    void add_topic(QString &topic_name);
 
-    Topic search_topic(QString topic_name);
+    void publish_message(QString &topic_name, QString &message);
 
-    void add_message(QString topic_name);
-
-    void add_sbscriber(QString topic_name);
-
-    void add_reaction(QString topic_name, QString reaction);
+    QString get_message(QString &topic_name);
+    void subscribe(QString &topic_name, Subscribe *subscriber);
+    void add_reaction(QString &topic_name, QString &reaction, Subscribe *subscriber);
 
 private:
     QHash<QString, Topic> topics;
+    Topic* search_topic(QString topic_name);
 
 };
 
